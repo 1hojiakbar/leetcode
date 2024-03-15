3264b96f99136ae97d0f5676a6c1a9e9764dca41
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-15 06:35:04 +0500

    add binary

---
+++ 1-junior/addBinary.js
@@ -0,0 +1,8 @@
+// link => https://leetcode.com/problems/add-binary/description/
+
+// Solution 1
+const addBinary = function (a, b) {
+  return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
+};
+
+console.log(addBinary("11", "1")); // 100
