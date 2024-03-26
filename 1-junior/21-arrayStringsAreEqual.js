5096f05fb2e98fe2ec3297f61d0d826b95923a1b
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-26 06:55:21 +0500

    array strings are equal solution 2

--- 1-junior/21-arrayStringsAreEqual.js
+++ 1-junior/21-arrayStringsAreEqual.js
@@ -10,3 +10,19 @@ const arrayStringsAreEqual = function (word1, word2) {
 };
 
 console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"])); // true
+
+// Solution 2
+// Runtime - 55ms / 38.38%
+// Memory - 49.04mb 46.02%
+const solution2 = function (word1, word2) {
+  let maxIndex = word1.join("").length;
+  let str1 = "";
+  let str2 = "";
+  for (let i = 0; i <= maxIndex; i++) {
+    if (word1[i] != undefined) str1 += word1[i];
+    if (word2[i] != undefined) str2 += word2[i];
+  }
+  return str1 == str2;
+};
+
+console.log(solution2(["ab", "c"], ["a", "bc"]));
