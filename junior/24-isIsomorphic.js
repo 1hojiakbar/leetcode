8471ea3d1f420b625aa7751d9b6d3abb26b09fd4
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-03-30 06:44:00 +0500

    is Isomorphic solution

--- junior/24-isIsomorphic.js
+++ junior/24-isIsomorphic.js
@@ -1,31 +1,18 @@
 // link => https://leetcode.com/problems/isomorphic-strings/description/
 
 // Solution 1
+// Runtime - 69ms / 22.78%
+// Memory - 54.35mb / 9.85%
 const isIsomorphic = function (s, t) {
-  const defValue = [s, t];
-  let bool = false;
+  let arr = [];
+  if (s.length !== t.length) return false;
   for (let i in s) {
-    let reg = new RegExp(`${s[i]}`, "g");
-    let regexp = new RegExp(`${t[i]}`, "g");
-    if (
-      s.match(reg).length > 1 &&
-      s.match(reg).length <= 2 &&
-      t.match(regexp).length > 1 &&
-      t.match(regexp).length <= 2
-    ) {
-      s = s.replace(s[s.indexOf(s[i])], s[s.indexOf(s[i], i)]);
-      t = t.replace(t[t.indexOf(t[i])], t[t.indexOf(t[i], i)]);
-      if (s === defValue[0] && t === defValue[1]) bool = true;
-    } else if (s === t) {
-      bool = true;
-    }
+    arr.push(s.indexOf(s[i]));
+    if (arr[i] !== t.indexOf(t[i])) return false;
   }
-  return bool;
+  return true;
 };
 
 console.log(isIsomorphic("egg", "add"));
-console.log(isIsomorphic("foo", "bar"));
-console.log(isIsomorphic("paper", "title"));
-console.log(isIsomorphic("a", "a"));
-console.log(isIsomorphic("aaeaa", "uuxyy"));
-// console.log(isIsomorphic());
+
+// #description: Meni bu yechim qoniqtirmadi bunga keyinroq yana boshqa yechim topaman
