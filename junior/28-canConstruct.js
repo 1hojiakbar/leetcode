cc6a2e35df858a56ee6398f69f1878ef62a32495
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-03 12:31:35 +0500

    can construct

--- junior/28-canConstruct.js
+++ junior/28-canConstruct.js
@@ -1,23 +1,15 @@
 // link => https://leetcode.com/problems/ransom-note/description/
 
 // Solution 1
+// Runtime - 78ms / 38.07%
+// Memory - 53.93mb / 24.60%
 const canConstruct = function (ransomNote, magazine) {
-  let rg = new RegExp(`[${ransomNote}]`, "g");
-  if (magazine.startsWith(ransomNote) && rg.test(magazine)) {
-    return ransomNote.split("").every((val, i) => val === magazine[i]);
-  } else if (
-    ransomNote.length == magazine.length &&
-    !magazine.startsWith(ransomNote)
-  ) {
-    return ransomNote
-      .split("")
-      .reverse()
-      .every((val, i) => val === magazine[i]);
-  } else {
-    return false;
+  let ar = magazine.split("");
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (ar.indexOf(ransomNote[i]) === -1) return false;
+    else ar.splice(ar.indexOf(ransomNote[i]), 1);
   }
+  return true;
 };
 
 console.log(canConstruct("a", "b")); // false
-console.log(canConstruct("aa", "ab")); // false
-console.log(canConstruct("aa", "aab")); // true
