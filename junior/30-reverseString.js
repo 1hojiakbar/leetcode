b020a0a05f634f9699d9e900751b2b28f29b65b3
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-05 05:47:40 +0500

    reverse string

---
+++ junior/30-reverseString.js
@@ -0,0 +1,10 @@
+// link => https://leetcode.com/problems/reverse-string/description/
+
+// Solution 1
+// Runtime - 68ms / 91.98%
+// Memory - 56.82mb / 68.21%
+const reverseString = function (s) {
+  return s.reverse();
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"])); // ["o","l","l","e","h"]
